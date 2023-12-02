8bfb5620e3ddd4ca7bd069e51676c4a75321cee1
msftnntn <152199184+msftnntn@users.noreply.github.com>
2023-12-02 18:12:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight, SVG);
-}
+let x, y, radius, angle, Speed, amplitude;
 
-function keyPressed(event){ // 엔터키로 저장하는 법
-    console.log(event.key);
-    if( event.key === "Enter") {
-        save("제목","svg")
-    }
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    x = 0;
+    y = 0;
+    radius = 200;
+    angle = 0;
+    Speed = 0.5;
+    amplitude = 250;
 }
 
 function draw() {
-  
-    background(200);
-    
-    fill(5,68,0);
-    noStroke();
-    ellipse(225,680,240,160); // 진한 잎
+    background(220);
+
+    translate(width / 2, height / 2);
+
+    // 1
+    angle += angularSpeed;
+
+    x = cos(radians(angle)) * radius;
+    y = sin(radians(angle)) * amplitude;
+
+    // 2
+    for (let i = -300; i <= 300; i += 20) {
+        let x = i;
+        let cosx = cos(map(x, -250, 250, radians(0 + angle), radians(360 + angle)));
+        let y = map(cosx, -1, 1, -amplitude, amplitude);
+        fill(255);
+        ellipse(x, y, 20);
+    }
+
+   
+    Speed *= 0.99;
+
+    amplitude *= 0.9995; 
 }
